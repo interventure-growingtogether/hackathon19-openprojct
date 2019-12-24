a3b7b4cdb841cf076c3b1c1d278ba97ce6a6570c
jelenadjindjic <jelena.djindjic@tamedia.ch>
2019-12-24 12:12:52 +0100

    added project component

---
+++ react/openprojct/src/components/projectWidget/ProjectWidget.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './ProjectWidget.scss';
+
+class ProjectWidget extends Component {
+
+  render() {
+    return (
+      <div className="projects-widget">
+
+        <div className="projects-wrapper">
+              Project
+        </div>
+      </div>
+    );
+  }
+}
+
+export default ProjectWidget;
+
+
