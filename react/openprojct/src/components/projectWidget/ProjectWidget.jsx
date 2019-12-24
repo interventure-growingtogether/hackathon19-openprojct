b94a8b44430c6cf436392734ec62acd41c7de348
jelenadjindjic <jelena.djindjic@tamedia.ch>
2019-12-24 13:18:24 +0100

    project card added

--- react/openprojct/src/components/projectWidget/ProjectWidget.jsx
+++ react/openprojct/src/components/projectWidget/ProjectWidget.jsx
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import './ProjectWidget.scss';
-
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
 class ProjectWidget extends Component {
 
   render() {
@@ -8,7 +11,37 @@ class ProjectWidget extends Component {
       <div className="projects-widget">
 
         <div className="projects-wrapper">
-              Project
+            <div className="project-card">
+                <Card className="card">
+                    <CardHeader
+                        className="card-header"
+                        title={(
+                            <div className="title-section">
+
+                                <Typography
+                                    className="project-name"
+                                    component="h2"
+                                    data-test="project-title"
+                                >
+                                    PROJECT
+                                </Typography>
+
+                            </div>
+                        )}
+                    />
+
+                    <CardContent className="card-content">
+                            <Typography
+                                className="description"
+                                color="textSecondary"
+                                data-test="project-description"
+                            >
+                                DESCRIPTION
+                            </Typography>
+                    </CardContent>
+
+                </Card>
+            </div>
         </div>
       </div>
     );
